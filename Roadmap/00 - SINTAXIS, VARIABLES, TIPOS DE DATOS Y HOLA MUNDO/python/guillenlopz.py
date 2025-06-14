e53b5a492801cc4afe867b0620e2845b98a3997b
Guillermo <guille_eze17@hotmail.com>
2025-06-13 23:43:51 -0300

    Create guillenlopz.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/guillenlopz.py
@@ -0,0 +1,29 @@
+Comentario con URL de Phyton https://www.python.org
+
+comentario en una linea
+
+"""
+comentario
+en varias 
+lineas
+"""
+
+'''
+comentario
+en varias 
+lineas
+'''
+
+my_variable = "mi variable"
+
+MY_CONSTANT = "Mi constante"
+
+my_int = 1
+my_float = 1.5
+my_bool = True 
+my_bool = False
+my_string = "mi cadena de texto
+my_other_sring = 'mi otra cadena de texto'
+
+print"¡Hola, Phyton!"
+
